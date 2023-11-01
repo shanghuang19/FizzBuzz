a449e5ee7355f38ae652e0d2c07d8a524262e9bb
Shang Huang <shuang19@slb.com>
2023-11-01 14:28:18 +0800

    test: add a test for multiple of 3 and multiple of 5

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -4,7 +4,22 @@
     {
         public string CheckFizzBuzz(int number)
         {
-            return number.ToString();
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
         }
     }
 }
